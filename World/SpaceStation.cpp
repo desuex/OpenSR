648b5fe90730c65c32248c9ff6540e9808da4fd2
kakadu <kakadu.hafanana@gmail.com>
2017-01-20 20:16:12 +0300

    Space Station can be shown

--- World/SpaceStation.cpp
+++ World/SpaceStation.cpp
@@ -16,6 +16,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include "ResourceManager.h"
 #include "SpaceStation.h"
 
 #include <QtQml>
@@ -29,6 +30,9 @@ const quint32 SpaceStation::m_staticTypeId = typeIdFromClassName(SpaceStation::s
 template<>
 void WorldObject::registerType<SpaceStation>()
 {
+    // It will be great to be able to use the enum in the script but I don't know how
+    // At the moment it works only in .qml files
+    qRegisterMetaType<SpaceStation::StationKind>("StationKind");
     qmlRegisterType<SpaceStation>("OpenSR.World", 1, 0, "SpaceStation");
 }
 
@@ -51,6 +55,7 @@ const QMetaObject* WorldObject::staticTypeMeta<SpaceStation>()
 }
 
 SpaceStation::SpaceStation(WorldObject *parent, quint32 id): MannedObject(parent, id)
+  , m_StationKind(StationKind::Unspecified)
 {
 }
 
@@ -67,5 +72,72 @@ QString SpaceStation::namePrefix() const
 {
     return tr("Space station");
 }
+
+SpaceStation::StationKind SpaceStation::Kind() const
+{
+    return m_StationKind;
+}
+
+StationStyle SpaceStation::style() const
+{
+    return m_style;
+}
+
+void SpaceStation::setStationKind(SpaceStation::StationKind kind)
+{
+    if (m_StationKind == kind)
+        return;
+
+    m_StationKind = kind;
+    emit StationKindChanged(kind);
+}
+
+void SpaceStation::setStyle(StationStyle style)
+{
+//    if (m_style == style)
+//        return;
+
+    m_style = style;
+    emit styleChanged(style);
+}
+
+QString StationStyle::texture() const
+{
+    return getData<Data>().texture;
+}
+
+void StationStyle::setTexture(const QString &texture)
+{
+    auto d = getData<Data>();
+    d.texture = texture;
+    setData(d);
+}
+
+// StationStyle Streaming
+QDataStream& operator<<(QDataStream & stream, const StationStyle& style)
+{
+    return stream << style.id();
+}
+
+QDataStream& operator>>(QDataStream & stream, StationStyle& style)
+{
+    quint32 id;
+    stream >> id;
+    ResourceManager *m = ResourceManager::instance();
+    Q_ASSERT(m != 0);
+    Resource::replaceData(style, m->getResource(id));
+    return stream;
+}
+
+QDataStream& operator<<(QDataStream & stream, const StationStyle::Data& data)
+{
+    return stream << data.texture;
+}
+
+QDataStream& operator>>(QDataStream & stream, StationStyle::Data& data)
+{
+    return stream >> data.texture;
+}
+
 }
 }
--- World/SpaceStation.h
+++ World/SpaceStation.h
@@ -21,15 +21,47 @@
 
 #include "World.h"
 #include "MannedObject.h"
+#include "Resource.h"
 
 namespace OpenSR
 {
 namespace World
 {
+
+class OPENSR_WORLD_API StationStyle: public Resource
+{
+    Q_GADGET
+    Q_PROPERTY(QString texture READ texture WRITE setTexture)
+
+public:
+    struct Data {
+        QString texture;
+    };
+
+    QString texture() const;
+    void setTexture(const QString& texture);
+};
+
+QDataStream& operator<<(QDataStream & stream, const StationStyle& style);
+QDataStream& operator>>(QDataStream & stream, StationStyle& style);
+QDataStream& operator<<(QDataStream & stream, const StationStyle::Data& data);
+QDataStream& operator>>(QDataStream & stream, StationStyle::Data& data);
+
 class OPENSR_WORLD_API SpaceStation: public MannedObject
 {
     Q_OBJECT
     OPENSR_WORLD_OBJECT
+
+    Q_PROPERTY(OpenSR::World::SpaceStation::StationKind StationKind READ Kind WRITE setStationKind NOTIFY StationKindChanged)
+    Q_PROPERTY(OpenSR::World::StationStyle style READ style WRITE setStyle NOTIFY styleChanged)
+
+public:
+    enum class StationKind {
+        Unspecified = 0,
+        RangerCenter, ScienceBase, MilitaryBase, BusinessCenter, MedicalCenter
+    };
+    Q_ENUM(StationKind)
+    // TODO: streaming operators
 
 public:
     Q_INVOKABLE SpaceStation(WorldObject *parent = 0, quint32 id = 0);
@@ -37,8 +69,26 @@ public:
 
     virtual quint32 typeId() const;
     virtual QString namePrefix() const;
+
+    SpaceStation::StationKind Kind() const;
+    OpenSR::World::StationStyle style() const;
+
+public slots:
+    void setStationKind(StationKind kind);
+    void setStyle(OpenSR::World::StationStyle style);
+
+signals:
+    void StationKindChanged(StationKind kind);
+    void styleChanged(OpenSR::World::StationStyle style);
+
+private:
+    StationKind m_StationKind;
+    OpenSR::World::StationStyle m_style;
 };
 }
 }
+
+Q_DECLARE_METATYPE(OpenSR::World::StationStyle)
+Q_DECLARE_METATYPE(OpenSR::World::StationStyle::Data)
 
 #endif // OPENSR_WORLD_SPACESTATION_H
